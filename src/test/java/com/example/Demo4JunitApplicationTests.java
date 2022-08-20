4ea1c3b7b7b14d9ccd0193bc0080fb077039104a
jackson <2310576952@qq.com>
2022-08-20 17:13:13 +0800

    master commit1

---
+++ src/test/java/com/example/Demo4JunitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class  Demo4JunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("matser-test");
+        System.out.println("test")
+        ;
+    }
+
+}
